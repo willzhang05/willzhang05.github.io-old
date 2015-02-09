1b5d2217cf54fb96109923d37232e1923e83d7cf
William Zhang <willzhang05@gmail.com>
2015-02-09 17:10:07 +0000

    Button margin fix

--- ttf/ttf-script.js
+++ ttf/ttf-script.js
@@ -1,5 +1,38 @@
 var n=0;
+
 function redirect(){
-    n++;
-    window.location.replace("slide" + n + ".html");
+    if (event.type == "keydown"){
+        if (event.keyCode == '39'){
+            alert(event.keyCode);
+            n+=1;
+            if (n>0){
+                setTimeout(window.location.replace("slide" + n + ".html"),500);
+                document.title = "Slide " + n;
+            }
+        
+        } else if (event.keyCode == '37'){
+            alert(event.keyCode);
+            n-=1;
+            if (n<=0){
+                setTimeout(window.location.replace("TTF_presentation.html"),500);
+                
+            } else {
+                setTimeout(window.location.replace("slide" + n + ".html"),500);
+                document.title = "Slide " + n;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
