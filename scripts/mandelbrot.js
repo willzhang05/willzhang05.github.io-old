085d119b4e029f625924d931f69d7e8bfb2353b1
William Zhang <willzhang05@gmail.com>
2015-05-06 01:03:52 +0000

    Added new stopmotion video

--- scripts/mandelbrot.js
+++ scripts/mandelbrot.js
@@ -46,15 +46,6 @@ var update = function(){
 	}
 };
 
-/*function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}*/
-
 function resetGen(){
 	xp = 0;
 	r = colorGen();
